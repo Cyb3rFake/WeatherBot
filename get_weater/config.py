ddc5c3adbbbd6bcf13c411269c0c4a3162b87def
Cyb3rFake <74620864+Cyb3rFake@users.noreply.github.com>
2023-11-07 16:32:02 +0300

    Update config.py

--- get_weater/config.py
+++ get_weater/config.py
@@ -1,3 +1,3 @@
 # api_key = '6792dd790ca31ed54c4e1a134e341d1c'
-api_key = '5da2fc8c5461f322eb1500562d7b365f'                 #weather_api
-TOKEN_BOT = '6027163212:AAETHdAK9Kui7iZm6bmti9232OGIDkyVPto' #telegarm_bot_1
+api_key = ''  #weather_api
+TOKEN_BOT = '' #telegarm_bot_1
