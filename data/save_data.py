bcdfaad4a27067ffadf199abf55579270173a4a8
Cyb3rFake <74620864+Cyb3rFake@users.noreply.github.com>
2023-11-07 16:43:18 +0300

    Update save_data.py

--- data/save_data.py
+++ data/save_data.py
@@ -9,18 +9,3 @@ def save_data(name, usr_id, city):
         data["data"].append(new_data)
         with open('data.json', 'w', encoding='utf8 ') as outfile:
             json.dump(data, outfile, ensure_ascii=False,indent=3)
-# save_city('Вася_хуем_красит','ебать_05','Сити_хуй_сосите')
-
-# names_lst = ['Вася', 'Петя', 'Жопа', 'Хуй', 'Залупа']
-#
-#
-# for i in range(3):
-#     name = random.choice(names_lst)
-#     new_data = {'name': name, 'id': random.randrange(100000, 999999),
-#                 'city': random.choice(['moskva', 'voronegh', 'tambov'])}
-#     with open('data.json', encoding='utf8') as f:
-#         data = json.load(f)
-#         data["data"].append(new_data)
-#         with open("data.json", 'w', encoding='utf8') as outfile:
-#             json.dump(data, outfile, ensure_ascii=False, indent=3)
-
